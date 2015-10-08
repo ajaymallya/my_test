7a67b1e8764d2929143cca6ad7ad110268678de7
ajaymallya <ajay.mallya@gmail.com>
2015-10-08 05:33:06 -0500

    Cleaned up index route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,11 @@
+import Ember from 'ember';
+
 var IndexRoute = Ember.Route.extend({
     model: function(params) {
-            console.log('params: ' + JSON.stringify(params));
-            return Ember.$.getJSON('http://localhost:3000/stock_data');
+      return Ember.$.getJSON('http://localhost:3000/stock_data');
     },
     actions: {
       getChart: function(context) {
-        console.log('In getChart');
-        console.log(this.get('controller.model').symbol);
         this.transitionTo('chart',this.get('controller.model').symbol);
       }
     }
